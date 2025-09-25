ffbc146ceae7dfef98ae2aee9827bb37c43bb98f
mallepalliharshavardhan <141028801+mallepalliharshavardhan@users.noreply.github.com>
2025-09-25 22:02:50 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/harsha-portofolio' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/harsha-portofolio' : ''
+}
+
+module.exports = nextConfig
